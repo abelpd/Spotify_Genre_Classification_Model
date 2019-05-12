fe93461bcec274ffce629997c8c12bd3799c23c6
David Abelseth <abelpd07@gmail.com>
2019-05-12 16:31:34 -0700

    fixed url for scrape

--- spotify_csv_scape.py
+++ spotify_csv_scape.py
@@ -1,15 +1,24 @@
 import csv
-import urllib2
+import requests
 from functions import daterange
 from datetime import date, timedelta
+from contextlib import closing
+
 
 #https://spotifycharts.com/regional/global/daily/2017-01-01
 #^^^^sample url of where I'll scrape
 url = 'https://spotifycharts.com/regional/global/daily/'
 
 begdate = date(2017,1,1)
-enddate = date(2019,5,1)
+enddate = date(2017,1,2)
+testenddate = date(2019,5,1)
 
 for single_date in daterange(begdate, enddate):
-    print (single_date.strftime("%Y-%m-%d"))
-
+    date_url = url + str(single_date.strftime("%Y-%m-%d")) + '/download/'
+    print (date_url)
+    with closing(requests.get(date_url, stream=True)) as r:
+        f = (line.decode('utf-8') for line in r.iter_lines())
+        reader = csv.reader(f, delimiter=',', quotechar='"')
+        for row in reader:
+            # Handle each row here...
+            print (row)
