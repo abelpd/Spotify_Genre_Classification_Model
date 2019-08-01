20dba2c29a78fee5655eb2f60536a5b23a56a261
David Abelseth <abelpd07@gmail.com>
2019-07-31 17:22:56 -0700

    updating files

--- Data/spotify_csv_scape.py
+++ Data/spotify_csv_scape.py
@@ -11,7 +11,7 @@ def daterange(start_date, end_date):
 #https://spotifycharts.com/regional/global/daily/2017-01-01
 #^^^^sample url of where I'll scrape
 url = 'https://spotifycharts.com/regional/global/daily/'
-writeurl = r"C:\Users\abels\Desktop\spotify_scrape\csv_archive\\"
+writeurl = r"C:\Users\abels\Desktop\spotify_scrape\data\Top_200_Spotify_Data\\"
 
 begdate = date(2017,2,3)
 enddate = date(2017,2,4)
@@ -21,11 +21,15 @@ for single_date in daterange(begdate, enddate):
     print (date_url)
 
     with closing(requests.get(date_url, stream=True)) as r:
-        f = (line.decode('utf-8') for line in r.iter_lines())
+        r.encoding = r.apparent_encoding
+        print (r.encoding)
+        f = (line.decode() for line in r.iter_lines())
+        #f = (line.decode('latin-1',errors='replace') for line in r.iter_lines())
         reader = csv.reader(f, delimiter=',', quotechar='"')
 
         for row in reader:
             row.append(str(single_date))
             with open(writeurl + str(single_date) + '.csv', 'a', newline='', encoding='utf_8') as w:
+                #with open(writeurl + str(single_date) + '.csv', 'a', newline='', encoding='latin-1') as w:
                 wr = csv.writer(w, quoting=csv.QUOTE_ALL)
                 wr.writerow(row)
